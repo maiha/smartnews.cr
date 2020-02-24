2e358a75fe473f19c58bbe44e97a4ffa893dfa7f
maiha <maiha@wota.jp>
2020-02-25 07:08:01 +0900

    logger: fix mem info for musl

--- src/cli/cmds/check.cr
+++ src/cli/cmds/check.cr
@@ -1,8 +1,13 @@
 Cmds.command "check" do
   task mem do
-    info = Pretty::MemInfo.process
+    if config.verbose?
+      info = Pretty::MemInfo.process(skip_invalid: true)
+    else
+      info = Pretty::MemInfo.process
+    end
     info.values.each do |key, val|
       puts "#{key}: #{val}"
     end
+    puts info.max.to_s
   end
 end
