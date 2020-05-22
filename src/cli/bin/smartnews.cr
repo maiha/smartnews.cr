36a5e0b3902e2445f140c51443581ea802f11746
maiha <maiha@wota.jp>
2020-05-23 00:23:07 +0900

    cmds-0.3.6

--- src/cli/bin/smartnews.cr
+++ src/cli/bin/smartnews.cr
@@ -80,13 +80,6 @@ class Cli::Main
     end
   end
 
-  private def pretty_tasks(cmd, prefix)
-    array = cmd.class.usages.map{|help| help.split(/\s*#\s*/, 2)}
-    Pretty.lines(array, delimiter: "  # ").chomp.split(/\n/).map{|line|
-      "%s%s %s %s" % [prefix, PROGRAM_NAME, cmd.class.cmd_name, line.strip]
-    }.join("\n")
-  end
-
   def on_error(err)
     case err
     when Cmds::Finished
@@ -96,20 +89,16 @@ class Cli::Main
       exit 1
     when Cmds::ArgumentError
       STDERR.puts red(err.to_s)
-      STDERR.puts cmd.class.pretty_usage(prefix: " ")
       exit 2
-    when Cmds::CommandNotFound
-      STDERR.puts show_usage
-      exit 3
-    when Cmds::TaskNotFound
-      task_names = cmd.class.task_names
-      STDERR.puts red("ERROR: Task Not Found (current: '#{err.name}')")
-      STDERR.puts "[possible tasks]"
-      STDERR.puts "  %s" % task_names.join(", ")
-      STDERR.puts
-      STDERR.puts "[examples]"
-      STDERR.puts pretty_tasks(cmd, prefix: "  ")
-      exit 4
+    when Cmds::Navigatable
+      STDERR.puts Cmds::Navigator.new.navigate(err)
+      if usages = cmd?.try(&.class.usages)
+        array = usages.map(&.text.split(/\s*#\s*/, 2))
+        STDERR.puts Pretty.lines(array, delimiter: "  # ").chomp.split(/\n/).map{|line|
+          "%s%s %s %s" % ["  ", PROGRAM_NAME, cmd.class.cmd_name, line.strip]
+        }.join("\n")
+      end
+      exit err.exit_code
     when Smartnews::Dryrun
       STDOUT.puts err.inspect
     when Smartnews::Denied, Smartnews::Error
